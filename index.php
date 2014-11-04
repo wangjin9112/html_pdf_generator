56e2a93205c659bcbcec67fef4145d029999dd98
root <root@ip-172-31-21-211.us-west-2.compute.internal>
2014-11-04 21:16:04 +0000

    example pdf downloaded

--- index.php
+++ index.php
@@ -1,7 +1,31 @@
 <?php
 
-echo "PDF GEN";
+function print_header($file_out)
+{
+   header('Content-type: application/pdf');
+   header('Content-Disposition: attachment; filename="'.$file_out.'"');
+   echo file_get_contents($file_out);
+   exit;  
+}
+
+if ( isset($argv[1])) 
+{
+$url = $argv[1]; 
+}
+else 
+{
+$url = $_GET['url']; 
+}
+
+echo "\n". $url."\n"; 
+$file_out = 'pdfs/'. md5( $url).".pdf"; 
+
+if ( file_exists($file_out) ) 
+{
+	print_header($file_out); 
+} 
 
 
-
+exec("/usr/local/bin/wkhtmltopdf  --orientation Landscape --page-size A3  ". $url." " . $file_out) ; 
+print_header($file_out);
 ?>
