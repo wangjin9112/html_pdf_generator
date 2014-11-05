a13994b221e0c60ef8b684dddc0e5448c48b2c7f
root <root@ip-172-31-21-211.us-west-2.compute.internal>
2014-11-05 16:16:04 +0000

    fixed invalid characters

--- index.php
+++ index.php
@@ -21,10 +21,7 @@ $file_out = 'pdfs/'. md5( $url).".pdf";
 
 //disable caching for speed checks
 //if ( file_exists($file_out) ) 
-{
-	print_header($file_out); 
-} 
-
+//	print_header($file_out); 
 
 $commd = "/usr/local/bin/wkhtmltopdf  --orientation Landscape --page-size A3  ". escapeshellcmd($url)." " . escapeshellcmd($file_out); 
 exec($commd);
